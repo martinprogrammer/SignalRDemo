efbddf9e37ca219231599b7a2a0f5aa5201dbb41
martinprogrammer <martinprogrammer>
2014-11-03 19:32:13 +0000

    SignalrR simple MVC demo
    invoking hub method from controller

---
+++ SignalRDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public void SendGoodbye()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<HelloHub>();
+            context.Clients.All.helloo("Goodbye");
+            
+        }
+    }
+}
---
+++ SignalRDemo/HelloHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace SignalRDemo
+{
+    public class HelloHub : Hub
+    {
+        public void SayHello()
+        {
+            Clients.All.hello("Hello World");
+            Thread.Sleep(1000);
+            Clients.All.hello("This is me coming back from sleep");
+            Thread.Sleep(1000);
+            Clients.All.hello("Monday morning - new week all over again");
+            Thread.Sleep(1000);
+            Clients.All.hello("Neeed to turn the heating off");
+
+        }
+
+        public void SayGoodbye()
+        {
+            Clients.All.hello("GoodbyeNow");
+        }
+    }
+}
